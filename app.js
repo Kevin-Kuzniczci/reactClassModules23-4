ab6aa5d5092504bb2bb3eacd1d7f117a218079e5
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-06-08 12:41:33 -0700

    reactClassModuleas23&4

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = 5000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {root: "public"});
+});
+
+app.listen(port, () => {
+    console.log("The server is successfully listening");
+});
